97c6f619e0b1f31fc56b82f37bd8c0264620e88f
Vedant Joshi <vedant.joshi@btech.christuniversity.in>
2026-01-21 22:28:43 +0530

    Delete Project 1 directory

--- Project 1/src/app/directives/status-highlight.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatusHighlight } from './status-highlight';
-
-describe('StatusHighlight', () => {
-  it('should create an instance', () => {
-    const directive = new StatusHighlight();
-    expect(directive).toBeTruthy();
-  });
-});
--- Project 1/src/app/models/booking.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Booking {
-  id?: number;
-  eventId: number;
-  eventTitle: string;
-  userEmail: string;
-  userName: string;
-  tickets: number;
-  bookingDate: Date;
-}
